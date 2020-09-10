528f53b6a152901412d3fecc3ae4451675b6d96e
grantxc5 <grantxc5@ksu.edu>
2020-09-10 09:10:32 -0500

    not runnable, but good chunk of the project is finished.

---
+++ inclasswork 9.10.2020/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inclasswork_9._10._2020
+{
+
+    public delegate void StateObs(State s);
+    public delegate void InputHandler(State state, string args);
+
+
+    /*
+    // Create a method for a delegate.
+    public static void DelegateMethod(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    // Instantiate the delegate.
+    Del handler = DelegateMethod;
+
+    // Call the delegate.
+    handler("Hello World");
+    */
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Credentialsd model = new Credentialsd("Alice", "Wonderland");
+            Controllerd controller = new Controllerd(model);
+            LogInD view = new LogInD(controller.handleEvents);
+            controller.registeredObs(view.DisplayState);
+
+            Application.Run(view);
+        }
+    }
+}
---
+++ inclasswork 9.10.2020/VIew.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogInD
+{
+    public partial class LoginForm : Form
+    {
+        private InputHandler inHandler;
+
+        public LogIn200(InputHandler);
+
+        /// <summary>
+        /// Listener to the Login button. It takes the user's input
+        /// for the username and password and pass the values to the
+        /// Controller along with the state the view is in.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UxLoginBtn_Click(object sender, EventArgs e)
+        {
+            String un = tbUserName.Text;
+            String up = tbPassword.Text;
+            Console.WriteLine(un + " " +up);
+            controller.handleEvents(State.GOTPASSWORD, un+":"+up);
+
+        }
+
+        /// <summary>
+        /// Links the View to the controller.
+        /// </summary>
+        /// <param name="c">The App's main controller object. Later
+        /// this shold be a delegate.</param>
+        public void SetController(Controller c)
+        {
+            controller = c;
+        }
+
+        /// <summary>
+        /// TO synch the View and the Controller objects.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            controller.handleEvents(State.START, "");
+        }
+
+        /// <summary>
+        /// This method helps avoid some user input propblems, and helps 
+        /// keep the GUI in the right state.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbUserName_TextChanged(object sender, EventArgs e)
+        {
+            controller.handleEvents(State.GOTUSERNAME, "");
+        }
+
+
+        /// <summary>
+        /// This method helps avoid some user input propblems, and helps
+        /// keep the GUI in the right state.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbPassword_TextChanged(object sender, EventArgs e)
+        {
+            uxLoginBtn.Enabled = true;
+        }
+
+    }
+}
